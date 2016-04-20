986d7d14acfe6ba8ca77d0297d5ae81fe24281e4
MathiasB <git@denbeke.be>
2016-04-20 14:34:04 +0200

    Access-Control-Allow-Origin

--- bullyanalyzer/bullyanalyzer.go
+++ bullyanalyzer/bullyanalyzer.go
@@ -28,6 +28,13 @@ func main() {
 	api.Use(rest.DefaultDevStack...)
 	router, err := rest.MakeRouter(
 		rest.Get("/post/#post", func(w rest.ResponseWriter, req *rest.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			post := strings.Join(strings.Split(req.PathParam("post"), "%20"), " ")
+			result := a.AnalyzePost(post)
+			w.WriteJson(&result)
+		}),
+		rest.Get("/post/#post/", func(w rest.ResponseWriter, req *rest.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			post := strings.Join(strings.Split(req.PathParam("post"), "%20"), " ")
 			result := a.AnalyzePost(post)
 			w.WriteJson(&result)
